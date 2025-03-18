457361fd59a796b8a50ea5f538865b22db2a38cd
rlqja <rrlqja2@naver.com>
2025-03-18 19:15:42 +0900

    fix: aws cors 설정

--- src/main/java/song/teamo3/config/WebConfig.java
+++ src/main/java/song/teamo3/config/WebConfig.java
@@ -13,8 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//                .allowedOriginPatterns("http://localhost:8080")
-                .allowedOriginPatterns("http://3.39.11.109:8080")
+                .allowedOriginPatterns("http://localhost:8080", "http://3.39.11.109:8080")
                 .allowedHeaders("*")
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name())
                 .allowCredentials(true);
